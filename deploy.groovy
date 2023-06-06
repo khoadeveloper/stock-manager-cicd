8e82aa3ffccf3795199919a6fe75087a6d0a6386
khoango <tuankhoa@plus.ovh>
2023-06-06 23:17:24 +0700

    deploy.groovy

--- deploy.groovy
+++ deploy.groovy
@@ -1,16 +1,20 @@
+def remote = [:]
+remote.name = 'stock-manager-dev'
+remote.host = '139.99.72.34'
+remote.user = 'ubuntu'
+remote.allowAnyHosts = true
+remote.passphrase = "123456"
+
 pipeline {
     agent any
 
-    def remote = [:]
-    remote.name = 'stock-manager-dev'
-    remote.host = '139.99.72.34'
-    remote.user = 'ubuntu'
-    remote.allowAnyHosts = true
-    remote.passphrase = "123456"
-
     stages {
         stage("Prepage image") {
-            sshCommand remote: remote, command: "ls -lrt"
+            steps {
+                script {
+                    sshCommand remote: remote, command: "ls -lrt"
+                }
+            }
         }
     }
 }
