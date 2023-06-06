53932e360493c9f164c7e9f61e0d3eb90f095804
khoango <tuankhoa@plus.ovh>
2023-06-06 23:56:56 +0700

    deploy.groovy

--- deploy.groovy
+++ deploy.groovy
@@ -6,17 +6,32 @@ remote.allowAnyHosts = true
 pipeline {
     agent any
 
-    environment {
-        SSH_CREDS = credentials('ssh stock-manager-dev')
+    parameters {
+        choice(choices: [
+                "admin",
+                "api",
+                "common",
+                "config",
+                "eureka",
+                "flickr",
+                "gateway",
+                "kiotviet",
+                "lazada",
+                "mailer",
+                "scheduler",
+                "sendo",
+                "tiki"
+        ], description: "Which service to deploy?", name: "service")
     }
 
-
     stages {
-        stage("Prepage image") {
+        stage("Pull image") {
             steps {
                 sshagent(credentials: ['ssh stock-manager-dev']) {
                     sh '''
-                          ssh -o StrictHostKeyChecking=no ubuntu@139.99.72.34 'ls -lrt'
+                          ssh -o StrictHostKeyChecking=no ubuntu@139.99.72.34 "
+                            docker pull 139.99.72.55:5000/khuyenstore/${service}:0.0.1-SNAPSHOT
+                          "
                       '''
                 }
                 /*script {
