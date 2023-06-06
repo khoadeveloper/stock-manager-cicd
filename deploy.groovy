65d3aeb4d0eff5fb4768ae7baef06491d489a59d
khoango <tuankhoa@plus.ovh>
2023-06-06 23:13:16 +0700

    deploy.groovy

--- deploy.groovy
+++ deploy.groovy
@@ -1,13 +1,14 @@
 pipeline {
     agent any
 
-    stages {
-        def remote = [:]
-        remote.name = 'stock-manager-dev'
-        remote.host = '139.99.72.34'
-        remote.user = 'ubuntu'
-        remote.allowAnyHosts = true
+    def remote = [:]
+    remote.name = 'stock-manager-dev'
+    remote.host = '139.99.72.34'
+    remote.user = 'ubuntu'
+    remote.allowAnyHosts = true
+    remote.passphrase = "123456"
 
+    stages {
         stage("Prepage image") {
             sshCommand remote: remote, command: "ls -lrt"
         }
