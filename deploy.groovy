eda33ad9a2e36a71fdab571852d03a41f976e5cf
khoango <tuankhoa@plus.ovh>
2023-06-07 08:47:04 +0700

    deploy.groovy

--- deploy.groovy
+++ deploy.groovy
@@ -1,5 +1,33 @@
 versions = fetchVersions(service)
 
+properties([
+        parameters([
+                [
+                        $class: 'CascadeChoiceParameter',
+                        choiceType: 'PT_SINGLE_SELECT',
+                        description: 'Active Choices Reactive parameter',
+                        filterLength: 1,
+                        filterable: true,
+                        name: 'choice2',
+                        randomName: 'choice-parameter-7601237141171',
+                        referencedParameters: 'choice1',
+                        script: [
+                                $class: 'GroovyScript',
+                                fallbackScript: [
+                                        classpath: [],
+                                        sandbox: false,
+                                        script: 'return ["error"]'
+                                ],
+                                script: [
+                                        classpath: [],
+                                        sandbox: false,
+                                        script: 'if(choice1.equals("aaa")){return [\'a\', \'b\']} else {return [\'aaaaaa\',\'fffffff\']}'
+                                ]
+                        ]
+                ]
+        ])
+])
+
 pipeline {
     agent any
 
@@ -20,33 +48,6 @@ pipeline {
                 "tiki"
         ], description: "Which service to deploy?", name: "service")
     }
-
-    options([
-            parameters([
-                    [
-                            $class: 'CascadeChoiceParameter',
-                            name: "version",
-                            choiceType: 'PT_SINGLE_SELECT',
-                            description: "Version to deploy",
-                            filterable: false,
-                            script: [
-                                    $class: 'GroovyScript',
-                                    fallbackScript: [
-                                            classpath: [],
-                                            sandbox: false,
-                                            script:
-                                                    'return[\'Could not get Env\']'
-                                    ],
-                                    script: [
-                                            classpath: [],
-                                            sandbox: false,
-                                            script:
-                                                    'return["Dev","QA","Stage","Prod"]'
-                                    ]
-                            ]
-                    ]
-            ])
-    ])
 
     stages {
         stage("Pull image") {
