db0d51221af4be2824c7dcd8576d7eff147fe144
khoango <tuankhoa@plus.ovh>
2023-06-07 12:10:32 +0700

    update deploy script

--- deploy.groovy
+++ deploy.groovy
@@ -66,12 +66,14 @@ pipeline {
     stages {
         stage("Stopping container") {
             steps {
-                sshagent(credentials: ['ssh stock-manager-dev']) {
-                    sh '''
+                catchError {
+                    sshagent(credentials: ['ssh stock-manager-dev']) {
+                        sh '''
                        ssh -o StrictHostKeyChecking=no ubuntu@139.99.72.34 "
                         docker ps | grep  -E 'khuyenstore/${service}'  |  awk '{print \\\$1}' | xargs docker stop
                        "
                     '''
+                    }
                 }
             }
         }
